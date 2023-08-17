d662dd111993e6ee4e96e21926918868cabd6a1e
unknown <mrsaqlainsyed.com>
2023-08-17 12:21:09 +0530

    Programthird.java

---
+++ ProgramThird.java
@@ -0,0 +1,11 @@
+class ProgramThird{
+	public static void main(String Args[])
+	{
+	String name="Pooja";
+	System.out.println("Welcome to EduBridge "+name);
+	int a=10;
+	int b=20;
+	int sum=a+b;
+	System.out.println("Sum of "+a+" and "+b+" is "+sum);
+	}
+}
